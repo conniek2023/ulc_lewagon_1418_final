a8444bc738e2f1a05a66b1012ab0100e7f9749cd
robert nitschke <rdn216@outlook.com>
2023-12-05 18:00:08 +0800

    events controller complete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,28 +1,52 @@
 class EventsController < ApplicationController
 
+  def new
+    @event=Event.new
+    authorize @event
+  end
+
   def create
-  @event=Event.new
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
+    authorize @event
   end
 
   def destroy
-
-  end
-
-  def new
-
+    @event=Event.find(params[:id])
+    @event.destroy
+    redirect_to me_path, status: :see_other
+    authorize @event
   end
 
   def edit
-
+    @event = Event.find(params[:id])
+    authorize @event
   end
 
   def update
-
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render :edit
+    end
+    authorize @event
   end
 
   def show
-
+    @event = Event.find(params[:id])
+    authorize @event
   end
 
+  private
+
+  def event_params
+    authorize @event
+    params.require(:event).permit(:title, :location, :description, :start_at, :end_at, { :images => []})
+  end
 
 end

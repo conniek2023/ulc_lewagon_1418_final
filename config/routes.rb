c2362dc7a389a2df1a7fd04a791b7306ac0a4f51
remy c <chenggedejing@outlook.com>
2023-12-05 12:02:27 +0800

    finished the routes things

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,17 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+
+  resources :communities do #community manager
+    resources :community_members, only: [:create] #community_member
+    resources :events, only: [:new, :create] do#event_manager
+      resources :event_members, only: [:create, :destory] #event_member
+    end
+  end
+
+ resources :events, only:[:edit, :update, :destory, :show]
+
+  get "/me", to: "users#me", as: :me
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")

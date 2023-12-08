2176bdc36bce5a7c387e1ef61593571abe89bb92
remy c <chenggedejing@outlook.com>
2023-12-08 17:32:43 +0800

    finished the basic function of pages,90%of user story

--- app/controllers/event_members_controller.rb
+++ app/controllers/event_members_controller.rb
@@ -3,14 +3,15 @@ class EventMembersController < ApplicationController
 
   def create
 
-    @event_member = current_user.event_members.build
+    @event_member = current_user.event_members.build(event_id:params[:event_id])
     authorize @event_member
+    p @event_member
     if @event_member.save
       flash[:notice] = "You have joined an event successfully."
-      redirect_to @community_member.community
+      redirect_to @event_member.event
     else
       flash[:notice] = "Failed"
-      redirect_to community_path(@community_member.community)
+      redirect_to event_path(@event_member.event)
     end
   end
 
--- app/models/event_member.rb
+++ app/models/event_member.rb
@@ -1,5 +1,5 @@
 class EventMember < ApplicationRecord
   belongs_to :user
   belongs_to :event
-  validates :user_id, uniqueness: { scope: :community_id}
+  validates :user_id, uniqueness: { scope: :event_id}
 end
